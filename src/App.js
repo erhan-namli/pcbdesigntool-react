8de8448b59592a7bfccc70790cfdbd17abcc5303
erhan-namli <05180000494@ogrenci.ege.edu.tr>
2022-08-25 22:03:11 +0300

    second commit

--- src/App.js
+++ src/App.js
@@ -21,6 +21,7 @@ function App() {
               <Route path='/' element={<HomePage/>}/>
               <Route path='/designer' element={<BoardDesignerPage/>}/>
 
+            {/*  some text */}
             </Routes>
         
         </BrowserRouter>
