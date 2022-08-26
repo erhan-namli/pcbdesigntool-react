2cef81f1e34f05c9a14ba5e03de87e58284daee7
emirhan5445 <emirhan.guleroglu.5445@gmail.com>
2022-08-26 19:34:59 +0300

    a few changes

--- src/Pages/LoginPage.js
+++ src/Pages/LoginPage.js
@@ -3,26 +3,29 @@ import './LoginPage.css';
 import { Link } from "react-router-dom";
 
 const LoginPage = () => {
+
+    
+
     return (
         <>
             <div className="App">
                 <div className="login-box">
-                    <h2>Log-In</h2>
+                    <h2>Login</h2>
                     <form>
                         <div className="user-box">
                             <input type="text" name="" required="" />
-                            <label>First Name</label>
+                            <label>Username</label>
                         </div>
                         <div className="user-box">
-                            <input type="text" name="" required="" />
-                            <label>Last Name</label>
+                            <input type="password" name="" required=""  />
+                            <label>Password</label>
                         </div>
                         <div>
                             <span></span>
                             <span></span>
                             <span></span>
                             <span></span>
-                            <Link to='/'>
+                            <Link to='/' id="">
                                 Submit</Link>
                             
                         </div>
