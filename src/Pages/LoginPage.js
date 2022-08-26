7ed5f9de1d03eacd4dcb3f6d4e76ff20fcb3d2b2
emirhan5445 <emirhan.guleroglu.5445@gmail.com>
2022-08-26 23:15:47 +0300

    google auth and some login page ui design change

--- src/Pages/LoginPage.js
+++ src/Pages/LoginPage.js
@@ -2,13 +2,37 @@ import React from 'react';
 import './LoginPage.css';
 import { Link } from "react-router-dom";
 
+import jwt_decode from 'jwt-decode';
+
+import {useEffect} from "react";
+
 const LoginPage = () => {
 
-    
+    function handleCallbackResponse(response) {
+        console.log("BU " + response.crediental)
+
+    var userObject = jwt_decode(response.crediental)
+
+    console.log(userObject)
+    }
+
+    useEffect(() => {
+        /* global google */
+        google.accounts.id.initialize({
+            client_id: "244496027610-c34d9scktqtfh114hgbh2hsis13dlg52.apps.googleusercontent.com",
+            callback : handleCallbackResponse
+        });
+
+        google.accounts.id.renderButton(
+            document.getElementById("signInDiv"),
+            {theme : "outline", size:"large"}
+        );
+    }, []);
 
     return (
         <>
             <div className="App">
+           
                 <div className="login-box">
                     <h2>Login</h2>
                     <form>
@@ -27,6 +51,8 @@ const LoginPage = () => {
                             <span></span>
                             <Link to='/' id="">
                                 Submit</Link>
+
+                                <div id="signInDiv">  </div>
                             
                         </div>
                     </form>
